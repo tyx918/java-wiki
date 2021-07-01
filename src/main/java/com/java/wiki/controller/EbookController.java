388908a2fdb650ce5c56aa36cfa903c8f5401579
谭勇祥 <2365501628@qq.com>
2021-07-01 21:07:08 +0800

    3-6 增加一个通用的返回类，用于接口的统一

---
+++ src/main/java/com/java/wiki/controller/EbookController.java
@@ -0,0 +1,30 @@
+package com.java.wiki.controller;
+
+import com.java.wiki.domain.Ebook;
+import com.java.wiki.resp.CommonResp;
+import com.java.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/* RestController 返回字符串
+* Controller 返回页面
+* */
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<List<Ebook>>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
+    }
+}
