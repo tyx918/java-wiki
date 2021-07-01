036ba7725254689005a2a11a7cce02171dfc63a1
谭勇祥 <2365501628@qq.com>
2021-07-01 16:10:17 +0800

    2-7 增加get和post的测试接口

--- src/main/java/com/java/wiki/controller/TestController.java
+++ src/main/java/com/java/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.java.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /* RestController 返回字符串
@@ -14,7 +15,12 @@ public class TestController {
     *
     */
     @RequestMapping("/hello")
-    public String hell0(){
+    public String hello(){
         return "hello world";
+    }
+
+    @PostMapping ("/hello/post")
+    public String helloPost(String name){
+        return "hello world POST, " + name;
     }
 }
