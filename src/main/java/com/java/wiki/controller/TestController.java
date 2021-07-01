3ef10afa4a8949650e48fcfd372ce46fbc56e6c8
谭勇祥 <2365501628@qq.com>
2021-07-01 16:18:55 +0800

    2-7 增加自定义配置项的示例

--- src/main/java/com/java/wiki/controller/TestController.java
+++ src/main/java/com/java/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.java.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 * */
 @RestController
 public class TestController {
-
+    /*自动读取自定义的配置项
+    * test.hello:TEST 这个是默认的配置值
+    * */
+    @Value("${test.hello:TEST}")
+    private String testHello;
     /*RequestMapping支持下面的所有请求 ， restful分格
     * get ,post , put , delete
     * @ return
@@ -16,7 +21,7 @@ public class TestController {
     */
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world," + testHello;
     }
 
     @PostMapping ("/hello/post")
